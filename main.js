0021bfef6a2c1edcf214c5567563fb2bf308de05
Miten99 <77421679+Miten99@users.noreply.github.com>
2021-11-27 11:45:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function OpenTab1() {
+    window.open("index1.html");
+}
+function OpenTab2() {
+    window.open("index1.html");
+}
+function OpenTab3() {
+    window.open("index1.html");
+}
+function OpenTab4() {
+    window.open("index1.html");
+}
+function OpenTab5() {
+    window.open("index1.html");
+}
